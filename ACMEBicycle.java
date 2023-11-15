8b51d7faf9b01717d6e8591ea64299423b5c4235
hanichris <hanichris71@gmail.com>
2023-11-15 23:17:26 +0000

    Rename Bicycle Class to ACMEBicycle. New class implements the `Bicycle` interface.

---
+++ ACMEBicycle.java
@@ -0,0 +1,27 @@
+class ACMEBicycle implements Bicycle {
+    
+    int cadence = 0;
+    int speed = 0;
+    int gear = 1;
+
+    public void changeCadence (int newValue) {
+        cadence += newValue;
+    }
+
+    public void changeGear (int newValue) {
+        gear += newValue;
+    }
+
+    public void speedUp (int increment) {
+        speed += increment;
+    }
+
+    public void applyBrakes (int decrement) {
+        speed -= decrement;
+    }
+
+    void printStatus () {
+        System.out.println("cadence: " + cadence + " speed: " + speed + 
+        " gear: " + gear);
+    }
+}
