d79e0b148b887e9ba79d9848e38d9e38b620224d
hanichris <hanichris71@gmail.com>
2023-11-16 20:21:44 +0000

    Declaring and initializing an array.

---
+++ ArrayDemo.java
@@ -0,0 +1,14 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+        byte[] anArray = new byte[10];
+
+        byte count = 0;
+
+        for (int i = 0; i < anArray.length; i++, count++) {
+            anArray[i] = count;
+        }
+        for (int i = 0; i < anArray.length; i++) {
+            System.out.println("Element at index " + i + ": " + anArray[i]);
+        }
+    }
+}
