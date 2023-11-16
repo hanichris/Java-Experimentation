1ba07dd4b36fcc2c7736933fe3bc34cb9af338b9
hanichris <hanichris71@gmail.com>
2023-11-16 21:21:12 +0000

    Using private fields and employing getter and setter functions to access and update their values respectively.

--- com/examples/bicycle/MountainBike.java
+++ com/examples/bicycle/MountainBike.java
@@ -1,14 +1,18 @@
 package com.examples.bicycle;
 
 public class MountainBike extends ACMEBicycle {
-    int seatHeight;
+    private int seatHeight;
 
     public MountainBike(int startHeight, int startCadence, int startGear, int startSpeed) {
         super(startCadence, startGear, startSpeed);
         seatHeight = startHeight;
     }
 
-    public void setHeight(int newValue) {
-        seatHeight += newValue;
+    public int getSeatHeight(){
+        return seatHeight;
+    }
+
+    public void setSeatHeight(int newValue) {
+        seatHeight = newValue;
     }
 }
