f893171c653cbc8804a85798cd8dd1266008e977
hanichris <hanichris71@gmail.com>
2023-11-17 19:04:43 +0000

    Exploring the use of a class with an overloaded constructor.

---
+++ CreateObjectDemo.java
@@ -0,0 +1,27 @@
+import com.examples.objects.Point;
+import com.examples.objects.Rectangle;
+
+public class CreateObjectDemo {
+    
+    public static void main(String[] args) {
+        Point originOne = new Point(23, 94);
+        Rectangle rectOne = new Rectangle(originOne, 100, 200);
+        Rectangle rectTwo = new Rectangle(50, 100);
+
+        // display rectOne's width, height, and area
+        System.out.println("Width of rectOne: " + rectOne.width);
+        System.out.println("Height of rectOne: " + rectOne.height);
+        System.out.println("Area of rectOne: " + rectOne.getArea());
+
+        rectTwo.origin = originOne;
+
+        // display rectTwo's position
+        System.out.println("X Position of rectTwo: " + rectTwo.origin.x);
+        System.out.println("Y Position of rectTwo: " + rectTwo.origin.y);
+
+        rectTwo.move(40, 72);
+
+        System.out.println("X Position of rectTwo: " + rectTwo.origin.x);
+        System.out.println("Y Position of rectTwo: " + rectTwo.origin.y);
+    }
+}
