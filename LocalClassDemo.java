742b624195530c95e6c708626324d76f4e6e48de
hanichris <hanichris71@gmail.com>
2023-11-19 19:24:04 +0000

    Showcasing the use of local classes in the definition of class method.

---
+++ LocalClassDemo.java
@@ -0,0 +1,8 @@
+import com.examples.objects.LocalClass;
+
+public class LocalClassDemo {
+    
+    public static void main(String... args) {
+        LocalClass.validatePhoneNumber("123-456-7890", "456-7890");
+    }
+}
