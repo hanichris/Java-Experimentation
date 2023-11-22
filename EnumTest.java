2c52247dbf15ede8e4ebd9c9dd0f7b9aa7624dad
hanichris <hanichris71@gmail.com>
2023-11-22 17:55:46 +0000

    Exploring the use of an `enum` type in a sample program.

---
+++ EnumTest.java
@@ -0,0 +1,38 @@
+import com.examples.objects.Day;
+
+public class EnumTest {
+    Day day;
+
+    public EnumTest(Day day) {
+        this.day = day;
+    }
+
+    public void tellItLikeItIs() {
+        switch (day) {
+            case MONDAY:
+                System.out.println("Mondays are bad");
+                break;
+            case FRIDAY:
+                System.out.println("Fridays are better");
+                break;
+            case SATURDAY: case SUNDAY:
+                System.out.println("Weekends are the best");
+                break;
+            default:
+                System.out.println("Midweeks are so-so");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        EnumTest firstDay = new EnumTest(Day.MONDAY);
+        firstDay.tellItLikeItIs();
+        
+        EnumTest thirdDay = new EnumTest(Day.WEDNESDAY);
+        thirdDay.tellItLikeItIs();
+
+        for (Day  day : Day.values()) {
+            System.out.println(day);
+        }
+    }
+}
