3a905097b79cad8d8a77c05ceb2e77ed6de66a60
hanichris <hanichris71@gmail.com>
2023-11-27 18:18:50 +0000

    Example of a generic class being defined.

---
+++ com/examples/generics/Box.java
@@ -0,0 +1,12 @@
+package com.examples.generics;
+
+/**
+ * Generic version of the Box class.
+ * @param <T> the type of the value being boxed.
+ */
+public class Box<T> {
+    private T t;
+
+    public void set(T t) { this.t = t; }
+    public T get() { return t; }
+}
