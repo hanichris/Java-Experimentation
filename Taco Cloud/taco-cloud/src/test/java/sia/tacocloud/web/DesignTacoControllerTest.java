e4d61416ac40b0efeef9b2dce523f61f567d50e0
hanichris <hanichris71@gmail.com>
2024-01-22 18:34:52 +0000

    Test the `DesignTacoController` controller.

---
+++ Taco Cloud/taco-cloud/src/test/java/sia/tacocloud/web/DesignTacoControllerTest.java
@@ -0,0 +1,24 @@
+package sia.tacocloud.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(DesignTacoController.class)
+public class DesignTacoControllerTest {
+    
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testDesignPage() throws Exception {
+        mockMvc.perform(get("/design"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("design"));
+    }
+}
