8cd641258d550cb167010d4c71295b3ae0b4bfbd
hanichris <hanichris71@gmail.com>
2024-01-23 15:59:04 +0000

    Validate the fields of the `Taco` object.

--- Taco Cloud/taco-cloud/src/main/java/sia/tacocloud/Taco.java
+++ Taco Cloud/taco-cloud/src/main/java/sia/tacocloud/Taco.java
@@ -3,8 +3,17 @@ package sia.tacocloud;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class Taco {
+
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long.")
     private String name;
+
+    @NotNull
+    @Size(min = 1, message = "You must choose at least 1 ingredient.")
     private List<Ingredient> ingredients;
 
     public Taco() {
