55398fe922cd2b6db06e5a5c5ee62b2013f08190
hanichris <hanichris71@gmail.com>
2024-01-25 21:48:17 +0000

    Inject the jdcTemplate as a dependency into the JdbcIngredientRepository class.

--- Taco Cloud/taco-cloud/src/main/java/sia/tacocloud/data/JdbcIngredientRepository.java
+++ Taco Cloud/taco-cloud/src/main/java/sia/tacocloud/data/JdbcIngredientRepository.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +14,7 @@ import sia.tacocloud.Ingredient;
 public class JdbcIngredientRepository implements IngredientRepository {
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
     public JdbcIngredientRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
