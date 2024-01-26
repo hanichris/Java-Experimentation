1c4c42661c01674f1bf3175867ed1e8a397d6f40
hanichris <hanichris71@gmail.com>
2024-01-26 21:26:48 +0000

    model representation of the Ingredient_Ref table that serves to link the `Taco` and `Ingredient` tables

---
+++ Taco Cloud/taco-cloud/src/main/java/sia/tacocloud/IngredientRef.java
@@ -0,0 +1,13 @@
+package sia.tacocloud;
+
+public class IngredientRef {
+    private final String ingredient;
+
+    public IngredientRef(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public String getIngredient() {
+        return this.ingredient;
+    }
+}
